dfe90b5c837ff10044b9dca67495c0e9fc80780b
Adarsh M <adarshmbr@gmail.com>
2017-07-30 19:54:44 +0530

    Initial commit

---
+++ BuildingEscape/Source/BuildingEsacape/BuildingEsacapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEsacapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESACAPE_API ABuildingEsacapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
